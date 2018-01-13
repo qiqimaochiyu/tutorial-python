54889aafa54a8dc58d9a33e8765ae32c66cba4cd
weiyu <848024931@qq.com>
2018-01-13 12:51:47 +0800

    Add files via upload

---
+++ wechat/wxpy1.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+import requests
+import wxpy
+import json
+
+robot = wxpy.Bot()
+def talks_robot(info='hello~'):
+    api_url = 'http://www.tuling123.com/openapi/api'
+    apikey = 'fa8cf679361b4321a793cd6492968769'
+    data = {'key':apikey,'info':info}
+    req = requests.post(api_url,data=data).text
+    replys = json.loads(req)['text']
+    return replys
+
+@robot.register()
+def reply_my_friend(msg):
+    message = '{}'.format(msg.text)
+    replys = talks_robot(info=message)
+    return replys
+
+robot.start()
+wxpy.embed()
