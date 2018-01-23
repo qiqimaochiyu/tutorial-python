2dcaf6c919adb335210e67da0e5bed2d81151970
weiyu <848024931@qq.com>
2018-01-23 19:20:57 +0800

    Update leetcode_396.py

--- leetcode/leetcode_396.py
+++ leetcode/leetcode_396.py
@@ -12,6 +12,7 @@ class Solution:
         :type A: List[int]
         :rtype: int
         """
+        ## 复杂度O（n^2）
         L = []
         for i in range(len(A)):
             l = A[i:] + A[:i]
@@ -23,3 +24,20 @@ class Solution:
         for i, j in enumerate(l):
             s += i * j
         return s
+
+            n=len(A)
+        if n==0:
+            return 0
+        
+    def maxRotateFunction2(self, A):
+        ## 复杂度 O（n）
+        s=0
+        sA=0
+        for i in range(n):
+            s+=A[i]*i
+            sA+=A[i]
+        maxf=s
+        for i in range(1,n):
+            s=s+sA-n*A[n-i]
+            maxf=max(maxf,s)
+        return maxf
