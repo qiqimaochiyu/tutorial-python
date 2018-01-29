d29e8cfdf669f4bea3ca260176fdee4ec08ed383
weiyu <848024931@qq.com>
2018-01-29 20:19:32 +0800

    Add files via upload

---
+++ leetcode/leetcode_228.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def summaryRanges(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[str]
+        """
+        ranges = []
+        for n in nums:
+            if not ranges or n > ranges[-1][-1] + 1:
+                ranges += [],
+            ranges[-1][1:] = n,
+        return ['->'.join(map(str, r)) for r in ranges]
