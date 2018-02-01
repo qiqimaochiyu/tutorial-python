6dd06e7ea49bee6cef44fae0cd9a73bd7aaa8ae1
weiyu <848024931@qq.com>
2018-02-01 12:28:58 +0800

    Create leetcode_448.py
    interesting ~~~

---
+++ leetcode/leetcode_448.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        for num in nums:
+            index = abs(num) - 1
+            nums[index] = -abs(nums[index])
+   
+        return [i+1 for i, num in enumerate(nums) if num > 0]
+        
