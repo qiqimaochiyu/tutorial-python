c6e4e9ade4dac9bbe0717a99ea1d7f68bd27d56a
weiyu <848024931@qq.com>
2018-02-12 14:04:42 +0800

    Create leetcode_139.py
    word break

---
+++ leetcode/leetcode_139.py
@@ -0,0 +1,11 @@
+class Solution:
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        ok = [True]
+        for i in range(1, len(s)+1):
+            ok += any(ok[j] and s[j:i] in wordDict for j in range(i)),
+        return ok[-1]
