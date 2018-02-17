0b952d783e0818d8d607d84fe65844da1edb676c
weiyu <848024931@qq.com>
2018-02-17 23:42:08 +0800

    Create leetcode_392.py

---
+++ leetcode/leetcode_392.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+                j += 1
+            else:
+                j += 1
+        if i == len(s):
+            return True
+        elif j == len(t):
+            return False
+        
