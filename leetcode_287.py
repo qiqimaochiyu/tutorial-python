d676c1fd324c131ae38a5609e6345529fb07200d
weiyu <848024931@qq.com>
2018-02-17 11:11:28 +0800

    Create leetcode_287.py

---
+++ leetcode_287.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findDuplicate(self, nums):
+        low = 0
+        high = len(nums) - 1
+        mid = (high + low) // 2
+        while high - low > 1:
+            count = 0
+            for k in nums:
+                if mid < k <= high:
+                    count += 1
+            if count > high - mid:
+                low = mid
+            else:
+                high = mid
+            mid = (high + low) // 2
+        return high
