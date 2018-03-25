0d3acdc85dcf8c5229b995662295ac8f134f3cd9
weiyu <848024931@qq.com>
2018-03-25 11:22:08 +0800

    Create leetcode_804.py

---
+++ leetcode/leetcode_804.py
@@ -0,0 +1,9 @@
+class Solution:
+    def uniqueMorseRepresentations(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        table = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        res = ["".join(i) for i in [[table[ord(w)-97] for w in word] for word in words]]
+        return len(set(res))
