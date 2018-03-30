4c54b6b5b7d75f5812e2d331be28f900ef69d406
weiyu <848024931@qq.com>
2018-03-30 20:19:02 +0800

    Create leetcode_211.py

---
+++ leetcode/leetcode_211.py
@@ -0,0 +1,30 @@
+class WordDictionary(object):
+    def __init__(self):
+        self.word_dict = collections.defaultdict(list)
+        
+
+    def addWord(self, word):
+        if word:
+            self.word_dict[len(word)].append(word)
+
+    def search(self, word):
+        if not word:
+            return False
+        if '.' not in word:
+            return word in self.word_dict[len(word)]
+        for v in self.word_dict[len(word)]:
+            # match xx.xx.x with yyyyyyy
+            for i, ch in enumerate(word):
+                if ch != v[i] and ch != '.':
+                    break
+            else:
+                return True
+        return False
+
+        
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
